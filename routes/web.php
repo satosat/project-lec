a142f79cfb4131fc79d6bbce87ae53dcdf239efc
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-11-06 22:25:36 +0700

    add delete and checkout route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/books');
 
 Route::get('/books', [BookController::class, 'index'])->name('home');
 Route::get('/books/{id}', [BookController::class, 'show'])->name('show book');
+Route::delete('/books/{id}', [BookController::class, 'delete'])->name('delete book');
+
+Route::get('/checkout/{id}', [TransactionController::class, 'show'])->name('checkout');
 
 
 // bawah ini jangan dihapus yaa
