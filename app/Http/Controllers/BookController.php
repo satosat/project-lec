a909a15bbcb6e8b6458d510feb4ffe66bc621523
vass26 <vasthy.tanisya@binus.ac.id>
2022-11-07 10:16:51 +0700

    commit

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -34,7 +34,7 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\BookDetailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin-add', function () {
+    return view('admin-add');
+});
+
+Route::post('/admin-add', [BookDetailController::class, 'addBook'])->name('addBook');
+
+
+Route::get('/admin-edit', function () {
+    return view('admin-edit');
 });
 
 
