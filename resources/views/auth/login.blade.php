561316a19f18d287fbbf55270b296af968ced1fa
Aradhana Indra Daniswara <aradhanaindradev@gmail.com>
2022-11-07 10:06:08 +0700

    Remove small thing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                 </div>
             @endif
 
-            <div class="mb-3">`
+            <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}"
                     placeholder="example@gmail.com" autofocus required>
